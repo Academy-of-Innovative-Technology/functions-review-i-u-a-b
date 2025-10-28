e0c7745b1b4cb2f80bcf5414022ba8b5cbbeb17a
u-a-b <uamayabanegas26@aoiths.org>
2025-10-28 14:02:19 -0400

    day1

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 // ===== FUNCTIONS WITH NO PARAMETERS ===== //
+function sayHello(){
+    let output1 = document.querySelector("#output1");
+    output1.innerHTML = "Hello Harry!"
+}
 
+function todaysDate(){
+let output1 = document.querySelector("#ouput1");
+output1.innerHTMl = "Today is tuesday, October 28th 2025";
+}
 
+function motivation(){
+let output1 = document.querySelector("#output1");
+output1.innerHTMl = "You're so cool!";
+}
 
-
-
-
-
-
-
-
+function rep(){
+let output1 = document.querySelector("#ouput1");
+output1.innerHTMl = "No one";
+}
 
 // ===== FUNCTIONS WITH PARAMETERS ===== //
-
+document.querySelector(".sayhellobtn").addEventListener("click", function(){
+    sayHello();
+});
 
 
 

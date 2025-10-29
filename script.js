31f893c18737478f173477213a8805c40138882b
u-a-b <uamayabanegas26@aoiths.org>
2025-10-29 13:44:05 -0400

    hjm8ut8hu

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // ===== FUNCTIONS WITH NO PARAMETERS ===== //
+
+
 function sayHello(){
     let output1 = document.querySelector("#output1");
     output1.innerHTML = "Hello Harry!"
@@ -6,30 +8,43 @@ function sayHello(){
 
 function todaysDate(){
 let output1 = document.querySelector("#ouput1");
-output1.innerHTMl = "Today is tuesday, October 28th 2025";
+output1.innerHTML = "Today is tuesday, October 28th 2025";
 }
 
 function motivation(){
 let output1 = document.querySelector("#output1");
-output1.innerHTMl = "You're so cool!";
+output1.innerHTML = "You're so cool!";
 }
 
 function rep(){
-let output1 = document.querySelector("#ouput1");
-output1.innerHTMl = "No one";
+    let output1 = document.querySelector("#ouput1");
+    output1.innerHTML = "No one";
 }
 
 // ===== FUNCTIONS WITH PARAMETERS ===== //
-document.querySelector(".sayhellobtn").addEventListener("click", function(){
-    sayHello();
-});
+function equation(){
 
-
-
-
+}
 
 
 
 
 
 // ===== EVENT LISTENERS THAT CALLS FUNCTIONS ===== //
+document.querySelector(".sayhellobtn").addEventListener("click", function(){
+    sayHello();
+});
+
+document.querySelector(".todaysdatebtn").addEventListener("click", function(){
+    todaysDate();
+});
+
+document.querySelector(".motivationbtn").addEventListener("click", function(){
+    motivation();
+});
+
+document.querySelector(".repbtn").addEventListener("click", function(){
+    rep();
+});
+
+
